85126ceb6bd160e37cb4d3ceea12999c89a1790d
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-04-06 15:35:27 +0300

    Update object.js

--- modules/object.js
+++ modules/object.js
@@ -1,14 +1,17 @@
-function isObject(obj, key) {
+/* eslint-disable eqeqeq */
+function isObject(obj) {
 	return obj != null && typeof obj === 'object' && !Array.isArray(obj);
 }
 
-const hasOwnProperty = Object.prototype.hasOwnProperty.call;
+exports.hasOwnProperty = function(target, key) {
+	return Object.prototype.hasOwnProperty.call(target, key);
+};
 
 exports.assignDeep = function(target, ...sources) {
 	if(isObject(target)) {
 		sources.forEach(source => {
 			if(isObject(source)) {
-				Object.keys(source).forEach(key => {
+				for(const key in source) {
 					if(isObject(source[key])) {
 						if(!(key in target)) {
 							Object.assign(target, { [key]: source[key] });
@@ -18,19 +21,19 @@ exports.assignDeep = function(target, ...sources) {
 					} else {
 						Object.assign(target, { [key]: source[key] });
 					}
-				});
+				}
 			}
 		});
 	}
 
 	return target;
-}
+};
 
 exports.assignDeepCheck = function(target, ...sources) {
 	if(isObject(target)) {
 		sources.forEach(source => {
 			if(isObject(source)) {
-				Object.keys(source).forEach(key => {
+				for(const key in source) {
 					if(isObject(source[key])) {
 						if(!(key in target)) {
 							if(!isObject(target[key])) throw new Error('Source value is object when target value is not');
@@ -42,23 +45,23 @@ exports.assignDeepCheck = function(target, ...sources) {
 						if(isObject(target[key])) throw new Error('Source value is not object when target value is');
 						Object.assign(target, { [key]: source[key] });
 					}
-				});
+				}
 			}
 		});
 	}
 
 	return target;
-}
+};
 
 exports.merge = function(...sources) {
 	return Object.assign({}, ...sources);
-}
+};
 
 exports.mergeDeep = function(...sources) {
 	const output = {};
 	sources.forEach(source => {
 		if(isObject(source)) {
-			Object.keys(source).forEach(key => {
+			for(const key in source) {
 				if(isObject(source[key])) {
 					if(!(key in output)) {
 						Object.assign(output, { [key]: source[key] });
@@ -68,18 +71,18 @@ exports.mergeDeep = function(...sources) {
 				} else {
 					Object.assign(output, { [key]: source[key] });
 				}
-			});
+			}
 		}
 	});
 
 	return output;
-}
+};
 
 exports.mergeDeepCheck = function(...sources) {
 	const output = {};
 	sources.forEach(source => {
 		if(isObject(source)) {
-			Object.keys(source).forEach(key => {
+			for(const key in source) {
 				if(isObject(source[key])) {
 					if(!(key in output)) {
 						if(!isObject(output[key])) throw new Error('Source value is object when target value is not');
@@ -91,90 +94,204 @@ exports.mergeDeepCheck = function(...sources) {
 					if(isObject(output[key])) throw new Error('Source value is not object when target value is');
 					Object.assign(output, { [key]: source[key] });
 				}
-			});
+			}
 		}
 	});
 
 	return output;
-}
+};
 
-exports.mapDeep = function(source, fn_map) {
+exports.map = function(source, fnMap) {
 	const output = {};
 	if(isObject(source)) {
-		Object.keys(source).forEach(key => {
-			if(isObject(source[key])) {
-				if(isObject(source[key])) {
-					output[key] = exports.mapDeep(output[key], fn_map);
-				} else {
-					Object.assign(output, { [key]: fn_map(source[key]) });
-				}
+		for(const key in source) {
+			Object.assign(output, { [key]: fnMap(source[key]) });
+		}
+	}
+	return output;
+};
+
+exports.filter = function(source, fnFilter) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			if(fnFilter(source[key])) {
+				Object.assign(output, { [key]: source[key] });
 			}
-		});
+		}
+	}
+	return output;
+};
+
+exports.filterMap = function(source, fnMap, fnFilter) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			if(fnFilter(source[key])) {
+				Object.assign(output, { [key]: fnMap(source[key]) });
+			}
+		}
+	}
+	return output;
+};
+
+function _mapDeep(source, fnMap) {
+	const output = {};
+	for(const key in source) {
+		if(isObject(source[key])) {
+			if(isObject(source[key])) {
+				output[key] = exports.mapDeep(output[key], fnMap);
+			} else {
+				Object.assign(output, { [key]: fnMap(source[key]) });
+			}
+		}
 	}
 	return output;
 }
 
-exports.filterDeep = function(source, fn_bool) {
+exports.mapDeep = function(source, fnMap) {
 	const output = {};
 	if(isObject(source)) {
-		Object.keys(source).forEach(key => {
-			if(isObject(source[key])) {
-				if(isObject(source[key])) {
-					output[key] = exports.filterDeep(output[key], fn_bool);
-				} else {
-					if(fn_bool(source[key])) {
-						Object.assign(output, { [key]: source[key] });
-					}
-				}
+		return _mapDeep(source, fnMap);
+	}
+	return output;
+};
+
+exports.mapKeys = function(source, fnMap) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			const newKey = fnMap(key);
+			if(newKey !== undefined) {
+				Object.assign(output, { [newKey]: source[key] });
+			} else {
+				Object.assign(output, { [key]: source[key] });
 			}
-		});
+		}
+	}
+};
+
+exports.filterKeys = function(source, fnFilter) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			if(fnFilter(key)) {
+				Object.assign(output, { [key]: source[key] });
+			}
+		}
+	}
+};
+
+exports.filterMapKeys = function(source, fnMap, fnFilter) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			const newKey = fnMap(key);
+			if(fnFilter(key)) {
+				Object.assign(output, { [newKey]: source[key] });
+			}
+		}
+	}
+};
+
+exports.mapInKeys = function(source, keyMap = {}) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			if(key in keyMap) {
+				Object.assign(output, { [keyMap[key]]: source[key] });
+			} else {
+				Object.assign(output, { [key]: source[key] });
+			}
+		}
+	}
+};
+
+exports.filterInKeys = function(source, keyMap = {}) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			if(key in keyMap) {
+				Object.assign(output, { [key]: source[key] });
+			}
+		}
+	}
+};
+
+exports.filterMapInKeys = function(source, keyMap = {}) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			if(key in keyMap) {
+				Object.assign(output, { [keyMap[key]]: source[key] });
+			}
+		}
+	}
+};
+
+function _filterDeep(source, fnFilter) {
+	const output = {};
+	for(const key in source) {
+		if(isObject(source[key])) {
+			if(isObject(source[key])) {
+				output[key] = exports.filterDeep(output[key], fnFilter);
+			} else if(fnFilter(source[key])) {
+				Object.assign(output, { [key]: source[key] });
+			}
+		}
 	}
 	return output;
 }
 
-exports.filterMapDeep = function(source, fn_map, fn_bool) {
+exports.filterDeep = function(source, fnBool) {
 	const output = {};
 	if(isObject(source)) {
-		Object.keys(source).forEach(key => {
-			if(isObject(source[key])) {
-				if(isObject(source[key])) {
-					output[key] = exports.filterMapDeep(output[key], fn_bool);
-				} else {
-					if(fn_bool(source[key])) {
-						Object.assign(output, { [key]: fn_map(source[key]) });
-					}
-				}
-			}
-		});
+		return _filterDeep(source, fnBool);
 	}
 	return output;
-}
+};
+
+exports.filterMapDeep = function(source, fnMap, fnFilter) {
+	const output = {};
+	if(isObject(source)) {
+		for(const key in source) {
+			if(isObject(source[key])) {
+				if(isObject(source[key])) {
+					output[key] = exports.filterMapDeep(output[key], fnFilter);
+				} else if(fnFilter(source[key])) {
+					Object.assign(output, { [key]: fnMap(source[key]) });
+				}
+			}
+		}
+	}
+	return output;
+};
 
 exports.cleanupDeep = function(source) {
-	return exports.filterDeep(source, (val) => {
+	return exports.filterDeep(source, val => {
 		if(val != null) return true;
 		return false;
 	});
-}
+};
 
-exports.flattenObject = function(target) {
+exports.flattenObject = function(source) {
 	const result = {};
 
-	for(const key in target) {
-		if(isObject(target[key])) {
-			const flatObject = exports.flattenObject(target[key]);
+	for(const key in source) {
+		if(isObject(source[key])) {
+			const flatObject = exports.flattenObject(source[key]);
 			for(const fkey in flatObject) {
-				if(!hasOwnProperty(flatObject, fkey)) continue;
+				if(!exports.hasOwnProperty(flatObject, fkey)) continue;
 
 				result[`${key}.${fkey}`] = flatObject[fkey];
 			}
 		} else {
-			result[key] = target[key];
+			result[key] = source[key];
 		}
 	}
 
 	return result;
-}
+};
 
 /**
  *
@@ -183,7 +300,7 @@ exports.flattenObject = function(target) {
  * @param {any} key1 First key
  * @param  {...any} keys Other keys
  */
- exports.setValRecursive = function(obj, val, key1, ...keys) {
+exports.setValRecursive = function(obj, val, key1, ...keys) {
 	if(keys.length === 0) {
 		obj[key1] = val;
 		return;
@@ -191,8 +308,8 @@ exports.flattenObject = function(target) {
 
 	if(obj[key1] == null) obj[key1] = {};
 
-	return exports.setValRecursive(obj[key1], val, ...keys);
-}
+	exports.setValRecursive(obj[key1], val, ...keys);
+};
 
 /**
  *
@@ -209,7 +326,7 @@ exports.getValRecursive = function(obj, key1, ...keys) {
 	if(obj[key1] == null) return undefined;
 
 	return exports.getValRecursive(obj[key1], ...keys);
-}
+};
 
 exports.delValRecursive = function(obj, key1, ...keys) {
 	if(keys.length === 0) {
@@ -219,7 +336,7 @@ exports.delValRecursive = function(obj, key1, ...keys) {
 	if(obj[key1] == null) return false;
 
 	return exports.delValRecursive(obj[key1], ...keys);
-}
+};
 
 /**
  * Traverses the object.
@@ -229,14 +346,14 @@ exports.delValRecursive = function(obj, key1, ...keys) {
  */
 exports.forEach = function(obj, func) {
 	for(const key in obj) {
-		if(hasOwnProperty(obj, key)) {
+		if(exports.hasOwnProperty(obj, key)) {
 			const result = func(key, obj[key]);
 			if(result !== undefined) {
 				obj[key] = result;
 			}
 		}
 	}
-}
+};
 
 /**
  *
@@ -254,4 +371,4 @@ exports.forEachVarRecursiveDepth = function(obj, func, depth = 0) {
 			}
 		}
 	}
-}
+};
