e0fd71fc5e40806f5060fdedd2643e160ad26622
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-05-08 06:09:45 +0300

    Add Math.clamp & Math.cycleClamp

--- modules/math.js
+++ modules/math.js
@@ -21,3 +21,27 @@ exports.mapRange = function(val, min1, max1, min2, max2) {
 exports.mapFunctionRange = function(fn, min1, max1, min2, max2) {
 	return (...params) => exports.mapRange(fn(...params), min1, max1, min2, max2);
 };
+
+exports.clamp = function(val, min, max) {
+	if(val < min) return min;
+	if(val > max) return max;
+	return val;
+};
+
+exports.cycleClamp = function(val, min, max) {
+	if(val < min) {
+		const dist = min - val;
+		const size = max - min;
+		const loops = dist / size;
+		const delta = loops - Math.floor(loops);
+		return max - (delta * size);
+	}
+	if(val > max) {
+		const dist = val - max;
+		const size = max - min;
+		const loops = dist / size;
+		const delta = loops - Math.floor(loops);
+		return min + (delta * size);
+	}
+	return val;
+};
