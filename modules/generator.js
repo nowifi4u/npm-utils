add4fde245e7de6f23592fa59cfeabc07ed8aecc
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-05-10 20:22:57 +0300

    Add generator functions

--- modules/generator.js
+++ modules/generator.js
@@ -10,3 +10,35 @@ exports.cycleGenerator = function*(genfunc, ...params) {
 		yield* generator;
 	}
 };
+
+/**
+ * @param {Generator} gen - Generator to be cycled
+ * @param {Function} func - Function to be passed the values to
+ */
+exports.forEach = function(gen, func) {
+	for(const val of gen) {
+		func(val);
+	}
+};
+
+/**
+ * @param {Generator} gen - Generator to be cycled
+ * @param {Function} func - Function to be passed the values to
+ * @param {?number} limit - Limit to the number of times the function can be called
+ */
+exports.forEachLimited = function(gen, func, limit = Infinity) {
+	let val = gen.next();
+	for(let idx = 0; idx < limit && !val.done; idx++) {
+		func(val.value, idx);
+	}
+};
+
+exports.rangeGenerator = function*({ min = 0, max = 0, step = 1 } = {}) {
+	if(step < 0) {
+		[min, max] = [max, min];
+		step = -step;
+	}
+	for(let idx = min; idx < max; idx += step) {
+		yield idx;
+	}
+};
