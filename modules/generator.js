7314913d328c42888b4fea99c25490a56da34aa0
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-05-10 20:22:05 +0300

    Update jsdoc and parameter names

--- modules/generator.js
+++ modules/generator.js
@@ -1,12 +1,12 @@
 /**
- * @param {*} gen - Generator to be cycles
+ * @param {GeneratorFunction} genfunc - Generator function to be cycled
  * @param  {...any} params - Parameters to be passed to the generator
  * @yields {*}
  * @generator
  */
-exports.cycleGenerator = function*(gen, ...params) {
+exports.cycleGenerator = function*(genfunc, ...params) {
 	while(true) {
-		const generator = gen(...params);
+		const generator = genfunc(...params);
 		yield* generator;
 	}
 };
