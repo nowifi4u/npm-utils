eb83dd48ea24766aeac901a78d6c9b8923c8a882
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-05-11 16:14:28 +0300

    Add allPossibleChunks generators

--- modules/array.js
+++ modules/array.js
@@ -51,3 +51,94 @@ exports.shuffleIndexes = function(size) {
 exports.shuffleIndexesGenerator = function*(size) {
 	yield* exports.shuffleIndexes(size);
 };
+
+// eslint-disable-next-line camelcase
+function* _impl_allPossibleChunksBigLittleGenerator(arr = [], istart = arr.length, pchain = []) {
+	if(istart <= 0) {
+		yield pchain;
+	} else {
+		for(let idx = istart; idx >= 1; idx--) {
+			const chain = [arr.slice(idx - 1, istart), ...pchain];
+			yield* _impl_allPossibleChunksBigLittleGenerator(arr, idx - 1, chain);
+		}
+	}
+}
+
+/**
+ * @generator
+ * @param {any[]} arr - Array to be used
+ * @param {number} istart - Start of the splitting
+ * @yields {any[][]}
+ */
+exports.allPossibleChunksBigLittleGenerator = function*(arr = [], istart = arr.length) {
+	yield* _impl_allPossibleChunksBigLittleGenerator(arr, istart);
+};
+
+// eslint-disable-next-line camelcase
+function* _impl_allPossibleChunksLittleBigGenerator(arr = [], istart = 0, pchain = []) {
+	if(istart < arr.length) {
+		for(let idx = istart; idx < arr.length; idx++) {
+			const chain = [...pchain, arr.slice(istart, idx + 1)];
+			yield* _impl_allPossibleChunksLittleBigGenerator(arr, idx + 1, chain);
+		}
+	} else {
+		yield pchain;
+	}
+}
+
+/**
+ * @generator
+ * @param {any[]} arr - Array to be used
+ * @param {number} istart - Start of the splitting
+ * @yields {any[][]}
+ */
+exports.allPossibleChunksLittleBigGenerator = function*(arr = [], istart = 0) {
+	yield* _impl_allPossibleChunksLittleBigGenerator(arr, istart);
+};
+
+// eslint-disable-next-line camelcase
+function* _impl_allPossibleChunksBigLittleGeneratorReverse(arr = [], istart = arr.length, pchain = []) {
+	if(istart <= 0) {
+		yield pchain;
+	} else {
+		for(let idx = 1; idx <= istart; idx++) {
+			const chain = [arr.slice(idx - 1, istart), ...pchain];
+			yield* _impl_allPossibleChunksBigLittleGeneratorReverse(arr, idx - 1, chain);
+		}
+	}
+}
+
+/**
+ * @generator
+ * @param {any[]} arr - Array to be used
+ * @param {number} istart - Start of the splitting
+ * @yields {any[][]}
+ */
+exports.allPossibleChunksBigLittleGeneratorReverse = function*(arr = [], istart = arr.length) {
+	yield* _impl_allPossibleChunksBigLittleGeneratorReverse(arr, istart);
+};
+
+// eslint-disable-next-line camelcase
+function* _impl_allPossibleChunksLittleBigGeneratorReverse(arr = [], istart = 0, pchain = []) {
+	if(istart < arr.length) {
+		for(let idx = arr.length; idx >= istart; idx--) {
+			const chain = [...pchain, arr.slice(istart, idx + 1)];
+			yield* _impl_allPossibleChunksLittleBigGeneratorReverse(arr, idx + 1, chain);
+		}
+	} else {
+		yield pchain;
+	}
+}
+
+/**
+ * @generator
+ * @param {any[]} arr - Array to be used
+ * @param {number} istart - Start of the splitting
+ * @yields {any[][]}
+ */
+exports.allPossibleChunksLittleBigGeneratorReverse = function*(arr = [], istart = 0) {
+	yield* _impl_allPossibleChunksLittleBigGeneratorReverse(arr, istart);
+};
+
+const gen = exports.allPossibleChunksBigLittleGeneratorReverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+require('./generator').forEach(gen, val => console.log(JSON.stringify(val)));
