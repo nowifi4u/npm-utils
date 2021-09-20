b126d71afc5fe3af7bbca2ce92292b5828b1e847
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-09-20 16:54:44 +0300

    Remove logger modules

--- main.js
+++ main.js
@@ -2,6 +2,3 @@ exports.array = require('./modules/array');
 exports.generator = require('./modules/generator');
 exports.math = require('./modules/math');
 exports.object = require('./modules/object');
-
-exports.idlogger = require('./modules/idlogger');
-exports.logger = require('./modules/logger');
